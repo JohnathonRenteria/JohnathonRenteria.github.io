bf2bb1666a19352ff34fcd7e9dbd5fa60d5186ab
JohnathonRenteria <110935171+JohnathonRenteria@users.noreply.github.com>
2022-12-02 18:35:05 -0600

    5

--- rectangle_area.js
+++ rectangle_area.js
@@ -4,7 +4,7 @@ let width = parseFloat(prompt("Enter Width of Rectangle"));
 
 let area = parseFloat((length)*(width));
 
-const html3 = `<p>Length = ${length}</p>
+let html3 = `<p>Length = ${length}</p>
                <p>Width = ${width}</p>
                <p>Area = ${area.toFixed(3)}</p`;
 document.write(html3);
