4718477a119d355f78870880dad5f88b6d813350
JohnathonRenteria <110935171+JohnathonRenteria@users.noreply.github.com>
2022-12-02 18:22:22 -0600

    5

--- rectangle_area.js
+++ rectangle_area.js
@@ -2,7 +2,7 @@ const length = parseFloat(prompt("Enter Length of Rectangle"));
 
 const width = parseFloat(prompt("Enter Width of Rectangle"));
 
-const area = parseFloat(length*width);
+const area = parseFloat((length)*(width));
 
 const html4 = `<p>Length = ${length}</p>
                <p>Width = ${width}</p>
